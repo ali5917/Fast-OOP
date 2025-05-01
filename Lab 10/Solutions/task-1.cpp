005235dededbc208903bc20009f2be9a9dda0563
Ali Kashif <alikashif5917@gmail.com>
2025-05-01 16:44:06 +0500

    add task 1

---
+++ Lab 10/Solutions/task-1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main () {
+    ifstream vehiclesFile;
+    vehiclesFile.open("vehicles.txt");
+    if (!vehiclesFile.is_open()) {
+        cout << "Error opening file!";
+    }
+
+    string thisLine;
+    while (getline(vehiclesFile, thisLine)) {
+        if (!thisLine.empty() && thisLine[0] != '/') {
+            cout << thisLine << endl;
+        }
+    }
+
+}
