62c96b15f018cf83daf198948086653166be7355
Ali Kashif <alikashif5917@gmail.com>
2025-05-01 22:20:27 +0500

    add task 4

---
+++ Lab 10/Solutions/task-4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main () {
+    ifstream vehiclesFile;
+    vehiclesFile.open("vehicles.txt");
+    if (!vehiclesFile.is_open()) {
+        cout << "Error opening file!";
+    }
+
+    string thisLine;
+    
+    while (getline(vehiclesFile, thisLine)) {
+        if (!thisLine.empty() && thisLine[0] != '/') {
+            stringstream ss(thisLine);
+            string type, id, name, year, extraData, certification;
+
+            getline(ss, type, ',');
+            getline(ss, id, ',');
+            getline(ss, name, ',');
+            getline(ss, year, ',');
+            getline(ss, extraData, ',');
+            getline(ss, certification);
+
+            int integerYear = stoi(year);
+
+            cout << "\nType: " << type << ", ID: " << id << ", Name: " << name << ", Year: " << integerYear << ", Extra Data: " << extraData << ", Certification: " << certification << endl;
+
+            size_t pos = extraData.find(':');
+            if (type == "Autonomous Car") {
+                if (pos != string::npos) {
+                    string str = extraData.substr(pos + 1);
+                    float swVersion = stof(str);
+
+                    cout << "ID: " << id << ", Software Version: " << swVersion << endl;
+                }
+                
+            } else if (type == "ElectricVehicle") {
+                if (pos != string::npos) {
+                    string str = extraData.substr(pos + 1);
+                    int capacity = stoi(str);
+                    cout << "ID: " << id << ", Battery Capacity: " << capacity << endl;
+                }
+            }
+        }
+    }
+}
