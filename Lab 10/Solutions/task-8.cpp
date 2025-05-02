fe16395389f1026de76221feae4cdcb11654bd33
Ali Kashif <alikashif5917@gmail.com>
2025-05-02 05:14:36 +0500

    add task 8

---
+++ Lab 10/Solutions/task-8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main () {
+    fstream file;
+    file.open("config.txt", ios::in | ios::out);
+    file.seekp(5, ios::beg);
+    file << "XXXXX";
+    file.close();
+}
