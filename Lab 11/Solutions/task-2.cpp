a2025cbe642b9a0cb79e2766b05f4089b0b2a51e
Ali Kashif <alikashif5917@gmail.com>
2025-05-04 12:36:06 +0500

    add task 2

--- Lab 11/Solutions/task-2.cpp
+++ Lab 11/Solutions/task-2.cpp
@@ -20,23 +20,55 @@ template<typename T>
 class Stack {
     private:
         int capacity;
-        T elements[capacity];
+        T *elements;
         int currentIndex;
 
     public:
-        Stack (int c) : currentIndex (0), capacity(c) {}
+        Stack (int c) : currentIndex (0), capacity(c) {
+            elements = new T [capacity];
+        }
+
+        ~Stack () {
+            delete [] elements;
+        }
 
         void push (T item) {
             if (currentIndex == capacity) {
                 throw StackOverflowException();
             } else {
                 elements[currentIndex] = item;
-                cout << "\nElement" << elements[currentIndex] << " pushed at index " << currentIndex;
+                cout << "\nElement " << elements[currentIndex] << " pushed at Index: " << currentIndex;
                 currentIndex ++;
             }
         }
 
-    public:
-
-
+        void pop () {
+            if (currentIndex == 0) {
+                throw StackUnderflowException();
+            } else {
+                cout << "\nElement " << elements[currentIndex - 1] << " popped at Index: " << currentIndex - 1;
+                currentIndex --;
+            }
+        }
 };
+
+int main () {
+    int capacity = 3;
+    Stack <double> myStack (capacity);
+    
+    try {
+        for (int i = 0; i < capacity + 1; i++) {
+            myStack.push(i + 2.4);
+        }
+    } catch (const StackOverflowException &e) {
+        cout << "\nPushing to a full Stack: " << e.what() << endl;
+    }
+    
+    try {
+        for (int i = 0; i < capacity + 1; i++) {
+            myStack.pop();
+        }
+    } catch (const StackUnderflowException &e) {
+        cout << "\nPopping from an empty Stack: " << e.what() << endl;
+    }
+}
