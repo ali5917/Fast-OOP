353c81fa0ad85c883f95b561c0c98c5e84ce92ac
Ali Kashif <alikashif5917@gmail.com>
2025-05-04 12:05:40 +0500

    add task 1

---
+++ Lab 11/Solutions/task-1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class InvalidValueException: public exception {
+
+    public:
+        const char *what () const noexcept override {
+            return "InvalidValueException - Age cannot be negative or exceeds 120";
+        }
+};
+
+void validateAge(int age) {
+    if (age < 0 || age > 120) {
+        throw InvalidValueException();
+    } else {
+        cout << "Age is valid\n";
+    }
+}
+
+int main () {
+    int age;
+    cout << "Enter age: ";
+    cin >> age;
+
+    try {
+        validateAge(age);
+    } catch (const InvalidValueException &e) {
+        cout << "Error: " << e.what() << endl;
+    }
+
+    return 0;
+}
